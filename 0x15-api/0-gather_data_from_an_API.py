ede74d59f687b4f54e408b0623882924b9494cd4
JackoDev <untaljacko@gmail.com>
2020-02-03 23:22:24 +0000

    [fix] task 0, fixed way of import for modules

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ a Python script that, using a REST API, for a given
-# employee ID, returns information about his/her TODO list progress. """
-from requests import get
-from sys import argv
+employee ID, returns information about his/her TODO list progress. """
+import requests
+import sys
 
 
 if __name__ == "__main__":
