4eb1ca3bd8272b013e61b943f6d9418e1cba50f6
JackoDev <untaljacko@gmail.com>
2020-02-03 23:44:24 +0000

    [fix] task 0, fixed way of import for modules

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 """ a Python script that, using a REST API, for a given
 employee ID, returns information about his/her TODO list progress. """
-import requests
-import sys
+from requests import get
+from sys import argv
+
+
+def get_from_api(id):
+    url = "https://jsonplaceholder.typicode.com/"
+    users = get(url + "users/{}".format(id)).json()
+    done = get(url + "todos?userId={}".format(id)).json()
+    new = []
+    for i in done:
+        if i.get("completed"):
+            new.append(i.get("title"))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(users["name"], len(new), len(done)))
+    for i in new:
+        print("\t {}".format(i))
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    fin = [f.get("title") for f in todo if f.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(fin), len(todo)))
-    [print("\t {}".format(i)) for i in fin]
+    get_from_api(int(argv[1]))
