51319f0ac60486ed51604e3e0db9c31817b2b484
JackoDev <untaljacko@gmail.com>
2020-02-04 00:14:31 +0000

    [fix] task 0, fixed way of import for modules

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,21 +4,22 @@ employee ID, returns information about his/her TODO list progress. """
 import requests
 from sys import argv
 
-
-todos = 'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]
-user = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
-todos = requests.get(todos)
-user = requests.get(user)
-u = user.json()
-t = todos.json()
-
-done = 0
-tasks = []
-for i in t:
-    if i['completed'] is True:
-        done += 1
-        tasks.append(i['title'])
-print("Employee {} is done with tasks({}/{}):".format(
-    u['name'], done, len(t)))
-for t in tasks:
-    print("\t {}".format(t))
+try:
+    todos = 'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]
+    user = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
+    todos = requests.get(todos)
+    user = requests.get(user)
+    u = user.json()
+    t = todos.json()
+    done = 0
+    tasks = []
+    for i in t:
+        if i['completed'] is True:
+            done += 1
+            tasks.append(i['title'])
+    print("Employee {} is done with tasks({}/{}):".format(
+        u['name'], done, len(t)))
+    for t in tasks:
+        print("\t {}".format(t))
+except Exception as error:
+    pass
