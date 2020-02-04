046866cfa1b38785119995fc12fea98e6ee3bfa9
JackoDev <untaljacko@gmail.com>
2020-02-04 00:55:18 +0000

    [fix] task 0, change way to solve

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,12 +10,13 @@ if __name__ == "__main__":
     url_user = "{}users/{}".format(source, sys.argv[1])
     req_user = requests.get(url_user)
     user_out = req_user.json()
-    print("Employee {} is done with tasks".format(user_out.get('name')), end="")
+    print("Employee {} is done with tasks".format(user_out.get('name')),
+          end="")
     task_list = '{}todos?userId={}'.format(source, sys.argv[1])
     req_user = requests.get(task_list)
     tasks = req_user.json()
     done = []
-    
+
     for task in tasks:
         if task.get('completed') is True:
             done.append(task)
