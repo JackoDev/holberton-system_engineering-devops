149d04a57f2bfd25c134f1f83175931884b76c85
JackoDev <untaljacko@gmail.com>
2020-02-04 00:11:49 +0000

    [fix] task 0, fixed way of import for modules

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """ a Python script that, using a REST API, for a given
 employee ID, returns information about his/her TODO list progress. """
-from requests import get
+import requests
 from sys import argv
 
 
-def get_from_api(id):
-    url = "https://jsonplaceholder.typicode.com/"
-    users = get(url + "users/{}".format(id)).json()
-    done = get(url + "todos?userId={}".format(id)).json()
-    new = []
-    for i in done:
-        if i.get("completed"):
-            new.append(i.get("title"))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(users["name"], len(new), len(done)))
-    for j in new:
-        print("\t {}".format(j))
+todos = 'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]
+user = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
+todos = requests.get(todos)
+user = requests.get(user)
+u = user.json()
+t = todos.json()
 
-
-if __name__ == "__main__":
-    get_from_api(int(argv[1]))
+done = 0
+tasks = []
+for i in t:
+    if i['completed'] is True:
+        done += 1
+        tasks.append(i['title'])
+print("Employee {} is done with tasks({}/{}):".format(
+    u['name'], done, len(t)))
+for t in tasks:
+    print("\t {}".format(t))
