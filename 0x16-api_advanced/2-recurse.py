f8431267ac7319a3b1e347a5b60b1fceaeca2cda
JackoDev <untaljacko@gmail.com>
2020-02-06 15:36:20 -0500

    [new] task 2 tested ok

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,26 +2,26 @@
 """
 a function that queries the Reddit API and returns the number of
 subscribers (not active users, total subscribers) for a given
-subreddit. If an invalid subreddit is given, the function should return 0.
+subreddit.
 """
 
 from requests import get
 
 
-def recurse(subreddit, hot_list=[], key1=""):
+def recurse(subreddit, hot_list=[], key_w=""):
     """
-    doc for top_ten posts of reddit
+    doc for recurse
     """
     headers = {"user-agent": "untaljacko"}
-    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, key1)
+    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, key_w)
     try:
         reqs = get(url, headers=headers, allow_redirects=False).json()
         best_ten = reqs["data"]["children"]
         for post in best_ten:
             hot_list.append(post['data']['title'])
-        key1 = reqs['data']['after']
-        if key1:
-            recurse(subreddit, hot_list, key1)
+        key_w = reqs['data']['after']
+        if key_w:
+            recurse(subreddit, hot_list, key_w)
         return (hot_list)
     except Exception:
         return ("None")
