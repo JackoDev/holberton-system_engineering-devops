8bdfbd43d13c58dff590034ff0d33f25cf3d47c1
JackoDev <untaljacko@gmail.com>
2020-02-19 12:14:49 -0500

    [new] adding readme and task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Using strace, find out why Apache is returning a 500 error. Once you find the issue,
+# fix it and then automate it using Puppet (instead of using Bash as you were previously doing).
+exec {
+  'phpp':
+  command => '/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
+}
